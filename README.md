4c35ee72d6a1b28c31f05978441b2597fe58cc6d
shelby89 <shelbynagliy@gmail.com>
2015-12-09 05:03:01 +0000

    ticket 1 ready for review 2

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # test_2
 Repo for test_2
 
-1. You need to create virtual environment.
+1. You need to create virtual environment and make "cd <your virtualenv>". 
 2. git clone git://github.com/dasap89/test_2.git
 3. cd test_2
-4. python db_create.py
-5. python db_migrate.py
-6. python run.py
+4. pip install -r requirements.txt
+5. python db_create.py
+6. python db_migrate.py
+7. python run.py
+8. Application will be avaliable at http://127.0.0.1:8080/list-notes
--- db_repository/versions/001_migration.py
+++ db_repository/versions/001_migration.py
@@ -5,22 +5,15 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
-note = Table('note', post_meta,
-    Column('id', Integer, primary_key=True, nullable=False),
-    Column('notes', String(length=255)),
-)
-
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['note'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    post_meta.tables['note'].drop()
