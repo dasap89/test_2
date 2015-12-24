4cef35acab428c5f74feadac15e2b38a872ee76b
shelby89 <shelbynagliy@gmail.com>
2015-12-24 10:28:27 +0000

    ticket 3 ajax added

--- app/models.py
+++ app/models.py
@@ -12,9 +12,13 @@ class Note(db.Model):
 
 class Request_to_App(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    request_time = db.Column(db.DateTime, default=datetime.datetime.now())
+    request_time = db.Column(db.DateTime)
     method = db.Column(db.String(5))
     path_info = db.Column(db.String(200))
     server_protocol = db.Column(db.String(10))
     server_address = db.Column(db.String(50))
     server_port = db.Column(db.String(5))
+    viewed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<Request to %r at %r>' % (self.path_info, self.request_time)
--- tests.py
+++ tests.py
@@ -8,10 +8,10 @@ from app.models import Note
 
 
 class TestCase(unittest.TestCase):
-    
+
     def setUp(self):
         app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')  # noqa
         self.app = app.test_client()
         db.create_all()
 
@@ -20,15 +20,15 @@ class TestCase(unittest.TestCase):
         db.drop_all()
 
     def test_main_page(self):
-        note1 = Note(notes = "First Note")
-        note2 = Note(notes = "Second Note")
+        note1 = Note(notes="First Note")
+        note2 = Note(notes="Second Note")
         db.session.add(note1)
         db.session.add(note2)
         db.session.commit()
         all_notes = Note.query.all()
         assert len(all_notes) > 0, u'The database has no entries'
         response = self.app.get('/list-notes')
-        assert response.status_code == 200, u'Status code is %s' % response.status_code
+        assert response.status_code == 200, u'Status code is not 200'
         assert response.data is not None, u'There is no data in response'
 
 if __name__ == '__main__':
