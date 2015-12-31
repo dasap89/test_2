450dd8bf7b7d68e1463768ef319f78d00f4021e5
shelby89 <shelbynagliy@gmail.com>
2015-12-31 06:47:24 +0000

    ticket 3 fix after review

--- tests.py
+++ tests.py
@@ -4,7 +4,7 @@ import unittest
 
 from app import app, db
 from config import basedir
-from app.models import Note
+from app.models import Note, Request_to_App
 
 
 class TestCase(unittest.TestCase):
@@ -27,9 +27,40 @@ class TestCase(unittest.TestCase):
         db.session.commit()
         all_notes = Note.query.all()
         assert len(all_notes) > 0, u'The database has no entries'
-        response = self.app.get('/list-notes')
+        response = self.app.get('/list-notes/')
         assert response.status_code == 200, u'Status code is not 200'
         assert response.data is not None, u'There is no data in response'
+
+    def test_middleware_save_requests(self):
+        response = self.app.get('/requests')
+        self.assertEqual(response.status_code, 301)
+        response = self.app.get('/requests/table/')
+        print response
+        self.assertEqual(response.status_code, 200)
+        print response.data
+        # self.assertEqual(len(response.context['new_requests']), 10)
+    
+    
+    def test_requests_page_return_10_records(self):
+        for i in range(0, 20):
+            response = self.app.get('/requests')
+            print response.data
+            self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['new_requests']), 10)
+
+    '''def test_if_requests_view_returns_valid_data(self):
+        response = self.app.get('/requests')
+        print response
+        self.assertEqual(response.status_code, 200)
+        content = response.context['new_requests'][0]
+        data = Requests.objects.first()
+        self.assertEqual(content, data)'''
+
+    def test_middleware_doesnt_save_ajax_requests(self):
+        self.app.get('/requests',
+                        headers=[('X-Requested-With', 'XMLHttpRequest')])
+        requsts_stored_in_db = len(Request_to_App.query.all())
+        self.assertEquals(requsts_stored_in_db, 0)
 
 if __name__ == '__main__':
     unittest.main()
